2df28b3e23efff8408f397ccd294be214c35cd0b
PravinRaja22 <102848025+PravinRaja22@users.noreply.github.com>
2023-02-21 11:01:09 +0530

    acount

--- controller/Account/accountInventory.js
+++ controller/Account/accountInventory.js
@@ -3,12 +3,19 @@ const Inventorywithaccount = async (request, reply) => {
     console.log("inside inventory with accounts lookup");
     console.log(request.query.searchId);
     try {
-       // var sql = "select * from deals a, inventory b where a.InventoryId= b._id and b._id = " + request.query.searchId;
-        var sql = "select a.* from account a, inventory b where a.InventoryId= b._id and b._id = "+ request.query.searchId;
+        // var sql = "select * from deals a, inventory b where a.InventoryId= b._id and b._id = " + request.query.searchId;
+        var sql = "select a.* from account a, inventory b where a.InventoryId= b._id and b._id = " + request.query.searchId;
 
-        let getDealsdata = await executeQuery(sql, [])
-        console.log(getDealsdata);
-        reply.send(getDealsdata)
+        let getAccountsdata = await executeQuery(sql, [])
+        console.log(getAccountsdata);
+        getAccountsdata.forEach(element => {
+
+            element.inventoryDetails = {
+                id: element.InventoryId,
+                propertyName: element.InventoryName
+            }
+        })
+        reply.send(getAccountsdata)
     }
     catch (err) {
         console.log('error in inventory with account lookup get')
