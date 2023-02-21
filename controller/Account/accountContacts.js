550a1f4831157edeee1bb79981cb77acdeb3d4c9
PravinRaja22 <102848025+PravinRaja22@users.noreply.github.com>
2023-02-21 12:52:48 +0530

    data

--- controller/Account/accountContacts.js
+++ controller/Account/accountContacts.js
@@ -6,6 +6,13 @@ const accountsContact = async (request, reply) => {
         var sql = "select a.* from contact a, account b where a.AccountId= b._id and b._id = " + request.query.searchId;
         let getaccountscontact = await executeQuery(sql, [])
         console.log(getaccountscontact);
+
+        getaccountscontact.forEach(element => {
+            element.accountDetails = {
+                accountName: element.AccountName,
+                id: element.AccountId
+            }
+        })
         reply.send(getaccountscontact)
     }
     catch (err) {
