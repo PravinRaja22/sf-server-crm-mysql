2779249bc73048099611aee2db039b12890d2015
PravinRaja22 <102848025+PravinRaja22@users.noreply.github.com>
2023-02-21 10:55:15 +0530

    test

--- controller/deals/dealwithinventory.js
+++ controller/deals/dealwithinventory.js
@@ -4,11 +4,13 @@ const inventorywithdeals = async (request, reply) => {
     console.log(request.query.searchId);
     try {
         // var sql = "select * from deals a, inventory b where a.InventoryId= b._id and b._id = " + request.query.searchId;
-        var sql = "select *,a._id from deals a, inventory b where a.InventoryId= b._id and b._id = " + request.query.searchId;
+        var sql = "select a.* from deals a, inventory b where a.InventoryId= b._id and b._id = " + request.query.searchId;
 
         let getDealsdata = await executeQuery(sql, [])
         console.log(getDealsdata);
         getDealsdata.forEach(element => {
+            console.log(element.modifiedDate)
+
             element.inventoryDetails = {
                 id:element.InventoryId,
                 propertyName : element.InventoryName
@@ -17,6 +19,8 @@ const inventorywithdeals = async (request, reply) => {
                 id:element.leadId,
                 leadName : element.leadName
             }
+            console.log("modified Date");
+            console.log(element.modifiedDate)
         })
         reply.send(getDealsdata)
     }
